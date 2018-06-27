2efff57d28e466772e191bdb0a1ee2eed4068aa3
zhaozhengwu <zhw.zhao@muheda.com>
2018-06-27 17:11:12 +0800

    Initial commit

---
+++ mynewlibrary/src/main/java/com/example/muheda/mynewlibrary/MainNewActivity.java
@@ -0,0 +1,13 @@
+package com.example.muheda.mynewlibrary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainNewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_new);
+    }
+}
